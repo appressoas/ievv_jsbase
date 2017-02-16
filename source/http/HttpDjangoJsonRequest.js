7a287180ad3d3aa6a37828551b3eed07763fca85
Espen A. Kristiansen <post@espenak.net>
2017-02-16 17:16:14 +0100

    HttpDjangoJsonRequest: Only request the csrftoken on post.

--- source/http/HttpDjangoJsonRequest.js
+++ source/http/HttpDjangoJsonRequest.js
@@ -6,26 +6,32 @@ import HttpCookies from "./HttpCookies";
  * of the Django csrftoken.
  */
 export default class HttpDjangoJsonRequest extends HttpJsonRequest {
-    /**
-     * @param args Same args as for {@link HttpResponse}.
-     */
-    constructor(...args) {
-        super(...args);
-        let cookies = new HttpCookies();
-        this.csrftoken = cookies.getStrict('csrftoken');
-    }
+  /**
+   * @param args Same args as for {@link HttpResponse}.
+   */
+  constructor(...args) {
+    super(...args);
+    this._cookies = new HttpCookies();
+  }
 
-    /**
-     * Ensures the csrftoken cookie value is automatically set in
-     * the ``X-CSRFToken`` header for all requests except GET and HEAD.
-     *
-     * @param method See {@link HttpRequest}.
-     */
-    setDefaultRequestHeaders(method) {
-        super.setDefaultRequestHeaders(method);
-        let shouldAddCsrfToken = !(method === 'GET' || method == 'HEAD');
-        if(shouldAddCsrfToken) {
-            this.setRequestHeader("X-CSRFToken", this.csrftoken);
-        }
+  /**
+   * Returns the value of the ``csrftoken`` cookie.
+   */
+  get csrftoken() {
+    return this._cookies.getStrict('csrftoken');
+  }
+
+  /**
+   * Ensures the csrftoken cookie value is automatically set in
+   * the ``X-CSRFToken`` header for all requests except GET and HEAD.
+   *
+   * @param method See {@link HttpRequest}.
+   */
+  setDefaultRequestHeaders(method) {
+    super.setDefaultRequestHeaders(method);
+    let shouldAddCsrfToken = !(method == 'GET' || method == 'HEAD');
+    if(shouldAddCsrfToken) {
+      this.setRequestHeader("X-CSRFToken", this.csrftoken);
     }
+  }
 }
