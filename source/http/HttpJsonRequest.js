11af2720c079d9f5cb4ff130851601efd5c81a02
Espen A. Kristiansen <t@espenak.net>
2018-04-22 12:21:46 +0200

    http...HttpJsonRequest: Support for getting all pagination pages (or many)
    in a single promise.

--- source/http/HttpJsonRequest.js
+++ source/http/HttpJsonRequest.js
@@ -1,5 +1,6 @@
 import HttpJsonResponse from "./HttpJsonResponse"
 import HttpRequest from "./HttpRequest"
+import ObjectManager from '../utils/ObjectManager'
 
 
 /**
@@ -42,5 +43,52 @@ export default class JsonHttpRequest extends HttpRequest {
     super.setDefaultRequestHeaders(method)
     this.setRequestHeader('Accept', 'application/json')
     this.setRequestHeader('Content-Type', 'application/json charset=UTF-8')
+  }
+
+  /**
+   * Shortcut for getting all pagination pages and resolve an array
+   * of the results for all pages.
+   *
+   * Assumes your API uses pagination where the ``next`` page is returned as
+   * an URL, and that the results for each pagination page is an array.
+   *
+   * The first page retrived is whatever this request is configured for,
+   * so you can use this to do things like request pagination page 10 -> 20,
+   * just ensure the request is configured so that the page that would have
+   * been retrieved if you just used ``get()`` would have been page 10.
+   *
+   * @param {string} resultsAttribute The attribute in the response data that
+   *    contains the results array. Defaults to ``results``.
+   * @param {string} nextUrlAttribute The attribute in the response data that
+   *    contains the URL of the next pagination page. Defaults to ``next``.
+   * @param {number} maxPages The max number of pages to load. If this is ``null``
+   *    (the default) we load all pages until the ``nextUrlAttribute`` value is ``null``
+   *    or ``undefined``.
+   * @returns {Promise<any>}
+   */
+  getAllPaginationPages (maxPages = null, resultsAttribute = 'results', nextUrlAttribute = 'next') {
+    return new Promise((resolve, reject) => {
+      const results = []
+      let loadedPageCount = 0
+      const recursiveLoadAllPagesFromApi = (request) => {
+        request.get()
+          .then((response) => {
+            results.push(...response.bodydata[resultsAttribute])
+            loadedPageCount += 1
+            let hasMaxPageCount = maxPages !== null && loadedPageCount >= maxPages
+            if (!hasMaxPageCount && ObjectManager.validate(response, 'bodydata', nextUrlAttribute)) {
+              const nextRequest = request.deepCopy()
+              nextRequest.setUrl(response.bodydata.next)
+              recursiveLoadAllPagesFromApi(nextRequest)
+            } else {
+              resolve(results)
+            }
+          })
+          .catch((error) => {
+            reject(error)
+          })
+      }
+      recursiveLoadAllPagesFromApi(this)
+    })
   }
 }
