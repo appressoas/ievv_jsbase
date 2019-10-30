67417be0e04f6ff75be8dc2581e6c6cbf72ef894
Vegard Angell <vegard@appresso.no>
2019-10-30 12:20:23 +0100

    MID-51 #comment add customError: IllegalOperationError

--- source/customErrors.js
+++ source/customErrors.js
@@ -21,6 +21,21 @@ export const ValueError = makeCustomError('ValueError')
 export const DoesNotExistError = makeCustomError('DoesNotExistError')
 
 /**
+ * Error meant for use when attempting to perform an operation when requirements are not met.
+ *
+ * @example <caption>Throwing the error</caption>
+ *  someAdminOnlyFunction () {
+ *    if (!this.isAdmin) {
+ *      throw new IllegalOperationError('someAdminOnlyFunction cannot be called from a non-admin state!')
+ *    }
+ *    // do some admin-stuff here.
+ *  }
+ *
+ * @type {Error}
+ */
+export const IllegalOperationError = makeCustomError('IllegalOperationError')
+
+/**
  * Error class meant to be used for something that is not yet implemented, e.g. when something should be implemented
  * in subclasses.
  *
